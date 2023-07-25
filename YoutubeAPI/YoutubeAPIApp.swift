e24aead70a9c21d9ad01be9585b6b18aa4545ad3
ZachMommaerts <zmommaerts@gmail.com>
2023-07-25 10:41:08 -0500

    Initial Commit

---
+++ YoutubeAPI/YoutubeAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  YoutubeAPIApp.swift
+//  YoutubeAPI
+//
+//  Created by Zach Mommaerts on 7/25/23.
+//
+
+import SwiftUI
+
+@main
+struct YoutubeAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
